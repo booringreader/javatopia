5ba0238e3d3a2215115098dc0517a763c346c161
booringreader <157519312+booringreader@users.noreply.github.com>
2024-04-10 11:03:20 +0530

    string reversal

--- strrev.java
+++ strrev.java
@@ -1,54 +1,54 @@
-
-// reversing a string using stack implementation with arraylists 
+// reversing a string using stack implementation with arraylist under the hood
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class strrev {
-    static class stack{
+    static class stack {
         // implement stack in ArrayList
         // push(), pop(), peek(), isEmpty() operations
 
         static ArrayList<Character> list = new ArrayList<>();
 
         // returns 1 if the size of stack object (arraylist on the floor) is empty
-        public static boolean isEmpty(){
+        public static boolean isEmpty() {
             return list.size() == 0;
         }
 
-        // push character datatype (since we'll deal with strings) into the stack (arraylist under the hood)
-        public static void push(char data){
+        // push character datatype (since we'll deal with strings) into the stack
+        // (arraylist under the hood)
+        public static void push(char data) {
             list.add(data);
         }
 
         // remove character datatype from the end of the arraylist (if not empty)
-        public static char pop(){
-            if(isEmpty()){
+        public static char pop() {
+            if (isEmpty()) {
                 System.out.println("stack empty");
                 return '\'';
             }
-            char output = list.get(list.size()-1);
-            list.remove(list.size()-1);
+            char output = list.get(list.size() - 1);
+            list.remove(list.size() - 1);
             return output;
         }
 
         // retrieve charater datatype from the arraylist (if not empty)
-        public static char peek(){
-            if(isEmpty()){
+        public static char peek() {
+            if (isEmpty()) {
                 System.out.println("stack empty");
                 return '\'';
             }
-            return list.get(list.size()-1);
+            return list.get(list.size() - 1);
         }
     }
 
     public static void reverse(String str, stack st) { // takes string and stack object as argument
-        char c; 
+        char c;
         StringBuilder out = new StringBuilder(); // initialized with a new StringBuilder
-        for(int i=0; i<str.length(); i++){
+        for (int i = 0; i < str.length(); i++) {
             c = str.charAt(i);
             st.push(c);
         }
-        while(!st.isEmpty()){
+        while (!st.isEmpty()) {
             out.append(st.pop());
         }
         System.out.println(out.toString()); // toString() converts StringBuilder to string
